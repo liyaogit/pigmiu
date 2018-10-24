fa8f2b6d125790c33b894110dca7df0198fb0ab0
yli <460255770@qq.com>
2018-10-24 10:25:28 +0800

    jvm相关

---
+++ src/main/java/com/jvm/oom/DirectMemoryOOM.java
@@ -0,0 +1,23 @@
+package com.jvm.oom;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * @Auther: yli
+ * @Date: 2018/8/29 17:05
+ * VM Args: -Xmx20M -XX:MaxDirectMemorySize=10M 若不指定MaxDirectMemorySize,默认与(-Xmx)一样
+ * @Description:怎么不报oom啊！
+ */
+public class DirectMemoryOOM {
+    private static final int _1MB = 1024 * 1024 * 3;
+    public static void main(String[] args) throws Exception{
+        Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+        unsafeField.setAccessible(true);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+        while (true){
+            unsafe.allocateMemory(_1MB);
+        }
+    }
+}
--- src/test/java/com/util/HttpUtilTest.java
+++ src/test/java/com/util/HttpUtilTest.java
@@ -26,7 +26,7 @@ public class HttpUtilTest {
 
     @Test
     public void testGet() throws ParseException, IOException {
-        String testUrl01 = "https://m.maoyan.com/information/45384";
+        String testUrl01 = "https://m.maoyan.com/information/48733";
         Map<String, Object> params01 = new HashMap<String, Object>();
         params01.put("参数01", "对应的参数取值");
         params01.put("参数02", "对应的参数取值");
@@ -104,5 +104,44 @@ public class HttpUtilTest {
 
         return qs;
     }
+
+
+    @Test
+    public void testKaiYan() throws ParseException, IOException {
+        String testUrl01 = "http://openapi.kaiyanapp.com/api/v1/videos";
+        String id ="aM2ZW5vZ2JseM9rkJO1X2FzZm2Ot2Rm9I1k5YU1M";
+        String key = "ksCey0rMDaOXWvh91IgWFl23ZhZeoW5p2l55Lk1";
+        Map<String, Object> params01 = new HashMap<String, Object>();
+
+        Calendar calendar = Calendar.getInstance();
+        long timestamp = 1539856380045L;
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+        long lastFetch = calendar.getTimeInMillis();
+
+        String token = DecryptUtil.MD5(id + key + timestamp);
+
+        params01.put("id", id);
+        params01.put("timestamp", timestamp);
+        params01.put("token", token);
+        params01.put("page", 5);
+        params01.put("lastFetch", lastFetch);
+        String result = HttpUtil.sendGet(testUrl01, params01);
+
+        String url = HttpUtil.sendGet("https://baobab.kaiyanapp.com/api/v1/playUrl?vid=132455&resourceType=video&editionType=default&source=aliyun&ptl=true", null);
+
+        Date start = new Date();
+        Document doc = Jsoup.parse(url);
+        Element content = doc.selectFirst("article");
+        //System.out.println(result);
+        System.out.println(content);
+        System.out.println(new Date().getTime()-start.getTime());
+
+        Element frame = content.selectFirst("iframe");
+
+
+        System.out.println(frame);
+        System.out.println(new Date().getTime()-start.getTime());
+    }
+
 
 }
